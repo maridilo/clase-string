0d6464204bf4a87a274303fbd4d45e0930c8b43d
maridilo <146822054+maridilo@users.noreply.github.com>
2023-11-23 12:22:15 +0100

    commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Crear una variable de tipo string
+    string phrase = "Hola, Mundo!";
+    // Imprimir la variable
+    cout << phrase << std::endl;
+    return 0;
+}
